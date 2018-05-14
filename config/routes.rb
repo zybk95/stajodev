fd87a4557ee65a96c3bfc5fd7228ad5abc669388
Abdulkadir ZEYBEK <abdulkadirzeybek95@gmail.com>
2018-05-14 23:42:16 +0300

    root sayfası olarak index.html.erb ayarlanır

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'number#index'
   get 'number/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
