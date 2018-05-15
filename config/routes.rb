92f381765cdfb7c76e879fa39013df676690e347
Abdulkadir ZEYBEK <abdulkadirzeybek95@gmail.com>
2018-05-16 00:42:48 +0300

    index metona için post istegi için routes dosyası duzenlenir

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'number#index'
+
   get 'number/index'
+  post 'number/index'
+  root 'number#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
