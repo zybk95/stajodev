d7221b7b56f6f400378c3dcd2109272c3db75a2d
Abdulkadir ZEYBEK <abdulkadirzeybek95@gmail.com>
2018-05-20 20:29:55 +0300

    numaranın tam dolduruluğ doldurulmadıgı kontrol edilir

--- app/controllers/number_controller.rb
+++ app/controllers/number_controller.rb
@@ -2,6 +2,33 @@ class NumberController < ApplicationController
 
   def index
       @no = params[:no]
-      return @no
+      @noi=@no.to_i
+      @nos=@no.to_s
+
+      if @noi.negative?
+        flash.now[:error] = "Girilen sayı pozitif olmalı"
+      end
+
+
+
+      array = @nos.to_s.split('').map(&:to_i)
+      array = array.map(&:to_i)
+      #arrayın elemanlar int olacak
+
+      if array.length < 4 && @no.nil? == false
+        flash.now[:error] = "Numarayı tam giriniz #{array.length}"
+
+      elsif array.length > 4
+        flash.now[:error] = "Fazla rakamı siliniz #{array[0].class}"
+      end
+
+      hesapla(array)
+
   end
+
+  def hesapla(array)
+
+  end
+
+
 end
