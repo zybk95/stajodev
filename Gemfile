c7b38a46970db87bc1ed9472b8e190808212c335
Abdulkadir ZEYBEK <abdulkadirzeybek95@gmail.com>
2018-05-21 13:56:08 +0300

    proje tasarım kısmı

--- Gemfile
+++ Gemfile
@@ -50,5 +50,8 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+gem 'bootstrap-sass', '~> 3.3.7'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/number_controller.rb
+++ app/controllers/number_controller.rb
@@ -9,17 +9,14 @@ class NumberController < ApplicationController
         flash.now[:error] = "Girilen sayı pozitif olmalı"
       end
 
-
-
       array = @nos.to_s.split('').map(&:to_i)
       array = array.map(&:to_i)
-      #arrayın elemanlar int olacak
 
       if array.length < 4 && @no.nil? == false
-        flash.now[:error] = "Numarayı tam giriniz #{array.length}"
+        flash.now[:error] = "Numarayı tam giriniz."
 
       elsif array.length > 4
-        flash.now[:error] = "Fazla rakamı siliniz #{array[0].class}"
+        flash.now[:error] = "Fazla rakamı siliniz."
       end
 
       hesapla(array)
@@ -28,25 +25,29 @@ class NumberController < ApplicationController
 
   def hesapla(array)
 
-    sozluk = [1071, 1903, 1905]
+    sozluk = [1071, 1453, 1881, 1903, 1905, 1907, 1920, 1923]
     sayi=array.inject{|n, d| n * 10 + d}
+    sayiyaz=array.join('')
 
-    if array.length == 0
+    if array.length < 4 || array.length > 4
       flash.now[:notice] = ""
     elsif array[0] == array [1] && array[0] == array [2] && array[0] == array[3]
-      flash.now[:notice] = "1. Şart"
+      flash.now[:notice] = "+90 850 885 #{sayiyaz} numarası özel bir numaradır. Fiyatı: ₺1000 + KDV"
     elsif array[1] == array [2] && array[2] == array[3]
-      flash.now[:notice] = "2. Şart"
+      flash.now[:notice] = "+90 850 885 #{sayiyaz} numarası özel bir numaradır. Fiyatı: ₺500 + KDV"
     elsif array[0] == array [2] && array[1] == array[3]
-      flash.now[:notice] = "2. Şart"
+      flash.now[:notice] = "+90 850 885 #{sayiyaz} numarası özel bir numaradır. Fiyatı: ₺500 + KDV"
     elsif array[0] == array [1] && array[2] == array[3]
-      flash.now[:notice] = "3. Şart"
+      flash.now[:notice] = "+90 850 885 #{sayiyaz} numarası özel bir numaradır. Fiyatı: ₺250 + KDV"
     elsif array[1] == 0 && array[3] == 0
-      flash.now[:notice] = "4. Şart"
-    elsif array[0] + array [3] == array[1] + array[2]
-      flash.now[:notice] = "4. Şart"
+      flash.now[:notice] = "+90 850 885 #{sayiyaz} numrası özel bir numaradır. Fiyatı: ₺100 + KDV"
+    elsif array[0] + array [3] == array[1] + array[2] && array[0] < array [1] && array[1] < array [2] && array[2] < array[3]
+      flash.now[:notice] = "+90 850 885 #{sayiyaz} numarası özel bir numaradır. Fiyatı: ₺100 + KDV"
     elsif sozluk.include?(sayi)
-      flash.now[:notice] = "5. Şart"
+      flash.now[:notice] = "+90 850 885 #{sayiyaz} numarası özel bir numaradır. Fiyatı: ₺50 + KDV"
+    else
+      flash.now[:notice] = "+90 850 885 #{sayiyaz} numarası normal bir numaradır."
     end
   end
+
 end
